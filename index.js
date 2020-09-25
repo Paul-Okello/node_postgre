d72dd059cf5248e34e208b2d739265918a584a31
Paul Okello <paulokello273@gmail.com>
2020-09-25 14:50:36 +0300

    create server

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require("http");
+
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const { url } = req;
+  console.log(url);
+  const translations = {
+    1: "One",
+    2: "Two",
+    3: "Three",
+  };
+  if (url == "/translations") {
+  }
+  res.setHeader("content-Type", "application/json");
+  res.write(JSON.stringify(translations));
+  res.end();
+
   res.end("Welcome to node");
 });
 
