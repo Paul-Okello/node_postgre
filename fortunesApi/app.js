0cb0841e5b23879e73e4010ec7c7a6e83c5c639e
Paul Okello <paulokello273@gmail.com>
2020-09-26 10:58:47 +0300

    Added post request

--- fortunesApi/app.js
+++ fortunesApi/app.js
@@ -17,4 +17,19 @@ app.get("/fortunes/:id", (request, response) => {
   response.json(fortunes.find((f) => f.id == request.params.id));
 });
 
+app.post("/fortunes", (request, response) => {
+  console.log(request.body);
+  const { message, luckyNumber, spiritAnimal } = request.body;
+
+  const fortuneIds = fortunes.map((f) => f.id);
+  const fortune = {
+    id: (fortuneIds.length > 0 ? Math.max(...fortuneIds) : 0) + 1,
+    message,
+    luckyNumber,
+    spiritAnimal,
+  };
+
+  const newFortunes = fortunes.concat(fortune);
+  response.json(newFortunes);
+});
 module.exports = app;
